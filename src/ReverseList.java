32793260aa63a507c147d0948216063c89b25c47
也许丶 <myyearn545@163.com>
2019-04-12 20:37:08 +0800

    反转链表

---
+++ src/ReverseList.java
@@ -0,0 +1,25 @@
+/**
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ */
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        // 尾节点
+        ListNode reverHead = null;
+        // 前一个结点
+        ListNode preNode = null;
+        ListNode currNode = head;
+        while (currNode != null){
+            ListNode next = currNode.next;
+            if(next != null){
+                reverHead = next;
+            }
+            currNode.next = preNode;
+            preNode = currNode;
+            currNode = next;
+        }
+        return reverHead;
+    }
+}
